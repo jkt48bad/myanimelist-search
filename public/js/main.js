1571a2d012f36a2dac400b6994f3163c86b102ec
Kyle Domaszewicz <kyledomaszewicz@gmail.com>
2018-09-17 00:58:19 -0700

    Adds logic to perform search based on the current input field and advanced options

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,95 @@
 "use strict";
+/**
+ * returns a string of parameter values from the checkboxes checked in advanced options
+ * @param ids An array of IDs attached to the checkboxes to get the state of
+ */
+function getCheckboxState(ids) {
+    var contentValue = '';
+    for (var i = 0; i < ids.length; i += 1) {
+        var $checkbox = $("#" + ids[i]);
+        if ($checkbox.is(':checked')) {
+            if (contentValue.length !== 0) {
+                contentValue += ',';
+            }
+            contentValue += $checkbox.val();
+        }
+    }
+    return contentValue;
+}
+/**
+ * builds a section of the URL parameter and returns it as a string. Will return
+ * an empty string if no values are to be sent.
+ * @param paramDetails Object representing the details of the parameter to be built
+ */
+function buildUrlParameter(paramDetails) {
+    var contentParam = "&" + paramDetails.name + "=";
+    var contentValue = getCheckboxState(paramDetails.inputs);
+    if (contentValue.length !== 0) {
+        return "" + contentParam + contentValue;
+    }
+    return '';
+}
+/**
+ * Builds the url to perform an AJAX search from MAL using Jikan
+ */
+function buildSearchUrl(page) {
+    var baseUrl = 'https://api.jikan.moe/v3/search/anime';
+    var searchParam = '?q=';
+    var searchValue = $('#search-box').val();
+    var contentParamDetails = {
+        name: 'type',
+        inputs: ['tvCheckBox', 'ovaCheckBox', 'movieCheckBox', 'specialCheckBox'],
+    };
+    var statusParamDetails = {
+        name: 'status',
+        inputs: ['airingCheckBox', 'completedCheckBox', 'upcomingCheckBox'],
+    };
+    var ratingParamDetails = {
+        name: 'rating',
+        inputs: [
+            'gCheckBox',
+            'pgCheckBox',
+            'pg13CheckBox',
+            'r17CheckBox',
+            'rCheckBox',
+            'rxCheckBox',
+        ],
+    };
+    var contentTypeParam = buildUrlParameter(contentParamDetails);
+    var statusParam = buildUrlParameter(statusParamDetails);
+    var ratingParam = buildUrlParameter(ratingParamDetails);
+    var pageParam = "&page=" + page;
+    return "" + baseUrl + searchParam + searchValue + contentTypeParam + statusParam + ratingParam + pageParam;
+}
+/**
+ * Gets search results from MAL
+ */
+function getSearchResults(page) {
+    var fullUrl = buildSearchUrl(page);
+    console.log(fullUrl);
+    fetch(fullUrl)
+        .then(function (response) {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Request failed!');
+    }, function (networkError) { return console.log(networkError.message); })
+        .then(function (jsonResponse) {
+        console.log(jsonResponse);
+    });
+}
 $('document').ready(function () {
-    $('#advanced-options').hide();
+    var $advancedOptions = $('#advanced-options');
+    $advancedOptions.hide();
     // toggles advanced options menu display
     $('#advanced-button').on('click', function () {
-        $('#advanced-options').slideToggle({
+        $advancedOptions.slideToggle({
             duration: 200,
             easing: 'swing',
         });
+    });
+    $('#search-button').on('click', function () {
+        getSearchResults('1');
     });
 });
 //# sourceMappingURL=main.js.map
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,11 +1,123 @@
+/**
+ * returns a string of parameter values from the checkboxes checked in advanced options
+ * @param ids An array of IDs attached to the checkboxes to get the state of
+ */
+function getCheckboxState(ids: string[]): string {
+    let contentValue: string = '';
+
+    for (let i = 0; i < ids.length; i += 1) {
+        const $checkbox = $(`#${ids[i]}`);
+
+        if ($checkbox.is(':checked')) {
+            if (contentValue.length !== 0) {
+                contentValue += ',';
+            }
+            contentValue += $checkbox.val();
+        }
+    }
+
+    return contentValue;
+}
+
+/**
+ * Interface for url parameter builder. Represents the input IDs of the checkboxes
+ * and the name of the param.
+ */
+interface ParamDetails {
+    name: string;
+    inputs: string[];
+}
+
+/**
+ * builds a section of the URL parameter and returns it as a string. Will return
+ * an empty string if no values are to be sent.
+ * @param paramDetails Object representing the details of the parameter to be built
+ */
+function buildUrlParameter(paramDetails: ParamDetails): string {
+    const contentParam: string = `&${paramDetails.name}=`;
+    const contentValue: string = getCheckboxState(paramDetails.inputs);
+    if (contentValue.length !== 0) {
+        return `${contentParam}${contentValue}`;
+    }
+
+    return '';
+}
+
+/**
+ * Builds the url to perform an AJAX search from MAL using Jikan
+ */
+function buildSearchUrl(page: string): string {
+    const baseUrl: string = 'https://api.jikan.moe/v3/search/anime';
+    const searchParam: string = '?q=';
+    const searchValue = $('#search-box').val();
+
+    const contentParamDetails: ParamDetails = {
+        name: 'type',
+        inputs: ['tvCheckBox', 'ovaCheckBox', 'movieCheckBox', 'specialCheckBox'],
+    };
+
+    const statusParamDetails: ParamDetails = {
+        name: 'status',
+        inputs: ['airingCheckBox', 'completedCheckBox', 'upcomingCheckBox'],
+    };
+
+    const ratingParamDetails: ParamDetails = {
+        name: 'rating',
+        inputs: [
+            'gCheckBox',
+            'pgCheckBox',
+            'pg13CheckBox',
+            'r17CheckBox',
+            'rCheckBox',
+            'rxCheckBox',
+        ],
+    };
+
+    const contentTypeParam: string = buildUrlParameter(contentParamDetails);
+    const statusParam: string = buildUrlParameter(statusParamDetails);
+    const ratingParam: string = buildUrlParameter(ratingParamDetails);
+    const pageParam: string = `&page=${page}`;
+
+    return `${baseUrl}${searchParam}${searchValue}${contentTypeParam}${statusParam}${ratingParam}${pageParam}`;
+}
+
+/**
+ * Gets search results from MAL
+ */
+function getSearchResults(page: string) {
+    const fullUrl: string = buildSearchUrl(page);
+
+    console.log(fullUrl);
+
+    fetch(fullUrl)
+        .then(
+            (response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error('Request failed!');
+            },
+            (networkError) => console.log(networkError.message)
+        )
+        .then((jsonResponse) => {
+            console.log(jsonResponse);
+        });
+}
+
 $('document').ready(() => {
-    $('#advanced-options').hide();
+    const $advancedOptions: JQuery<HTMLElement> = $('#advanced-options');
+
+    $advancedOptions.hide();
 
     // toggles advanced options menu display
     $('#advanced-button').on('click', () => {
-        $('#advanced-options').slideToggle({
+        $advancedOptions.slideToggle({
             duration: 200,
             easing: 'swing',
         });
+    });
+
+    $('#search-button').on('click', () => {
+        getSearchResults('1');
     });
 });

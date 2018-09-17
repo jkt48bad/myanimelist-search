7489360d13db61c6d9e5194ff8b26263d70c59c2
Kyle Domaszewicz <kyledomaszewicz@gmail.com>
2018-09-16 19:23:29 -0700

    Adds initial advanced options menu and styling for checkboxes

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,12 @@
 "use strict";
-console.log('Hello, world!');
+$('document').ready(function () {
+    $('#advanced-options').hide();
+    // toggles advanced options menu display
+    $('#advanced-button').on('click', function () {
+        $('#advanced-options').slideToggle({
+            duration: 200,
+            easing: 'swing',
+        });
+    });
+});
 //# sourceMappingURL=main.js.map
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,11 @@
-console.log('Hello, world!');
+$('document').ready(() => {
+    $('#advanced-options').hide();
+
+    // toggles advanced options menu display
+    $('#advanced-button').on('click', () => {
+        $('#advanced-options').slideToggle({
+            duration: 200,
+            easing: 'swing',
+        });
+    });
+});
